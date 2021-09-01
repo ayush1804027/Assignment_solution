84f79f0596baa7cf6e270b73c24aa1773b0df21e
Ayush Kumar <59093359+ayush1804027@users.noreply.github.com>
2021-09-01 08:06:16 +0530

    Required codes to make simple get API call

---
+++ app.js
@@ -0,0 +1,60 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const app=express();
+
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+class Result {
+  constructor(title, link) {
+    this.title = title;
+    this.link = link;
+  }
+}
+var res=[];
+var obj;
+const url = 'https://time.com/';
+axios(url).then(function(html){
+    //success!
+  html=html.data;
+  const $ = cheerio.load(html);
+  $('.latest h2 a').each((i,el) => {
+    const title= $(el).text();
+    const link=url + $(el).attr('href');
+    res.push(new Result(title,link));
+  });
+  obj= JSON.stringify(res);
+  console.log(obj);
+  })
+  .catch(function(err){
+    //handle error
+    console.log(err);
+  });
+
+
+  app.route("/getTimeStories")
+
+  .get(function(req, res){
+    res.end(obj);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running at port 3000 .");
+});
